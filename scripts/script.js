ab8305bc523274d96066ee7fe2ddd902ff861409
SophiaDelenardo1998 <70689522+SophiaDelenardo1998@users.noreply.github.com>
2022-05-23 14:44:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,26 @@
 
 
 var hetVliegtuig = document.querySelector(".vliegtuig");
-var deButton = document.querySelector("button");
+var deButton = document.querySelector(".button1");
 
 
 deButton.addEventListener("click", ikVlieg);
 
 function ikVlieg() {
 	hetVliegtuig.classList.toggle("vliegen");
+
+}
+
+var hetVliegtuig1 = document.querySelector(".vliegtuig1");
+var hetVliegtuig = document.querySelector(".vliegtuig");
+var deButton2 = document.querySelector(".button2");
+
+
+deButton2.addEventListener("click", ikVliegtwee);
+
+function ikVliegtwee() {
+  hetVliegtuig.classList.toggle("vliegenweg");
+	hetVliegtuig1.classList.toggle("vliegentwee");
+
+
 }
